aeeb52b31209bc40e261a35527940737bd6eef6f
RSathvika20 <7vikarodda@gmail.com>
2025-03-10 11:04:01 +0530

    Add files via upload

---
+++ JavaScript/lesson1(Javascript Basics)/assignment1.js
@@ -0,0 +1,37 @@
+//1a
+alert("Good morning");
+undefined;
+//1b
+alert(" Rodda Sathvika");
+undefined;
+//1c
+10 + 5;
+15;
+//1d
+20 - 5;
+15;
+//1e
+2 + 2 - 5;
+-1;
+//1f
+document.body.innerHTML = "Good morning";
+("Good morning");
+//1g
+document.body.innerHTML = "Rodda Sathvika";
+("Rodda Sathvika");
+//1h
+let tshirt = 10;
+let socks = 8;
+let dinnerplates = 20;
+t_shirt + socks + dinnerplates;
+38;
+//1i
+let account = 100;
+let spend = 20;
+let dinner = 50;
+let job = 200;
+account - spend - dinner + job;
+230;
+//1j
+document.body.innerHTML = " ";
+(" ");
