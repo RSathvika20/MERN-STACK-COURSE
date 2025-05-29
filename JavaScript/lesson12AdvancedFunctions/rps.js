a60363d7c6f3f0cab5c7128e1488629035a64797
RSathvika20 <7vikarodda@gmail.com>
2025-05-29 20:38:05 +0530

    modify

--- JavaScript/lesson12AdvancedFunctions/rps.js
+++ JavaScript/lesson12AdvancedFunctions/rps.js
@@ -19,6 +19,10 @@ if (!score) {
 let isAutoPlaying = false;
 let intervalId;
 
+//const autoPlay(){
+
+//}
+
 function autoPlay() {
   if (!isAutoPlaying) {
     intervalId = setInterval(() => {
@@ -31,18 +35,6 @@ function autoPlay() {
     isAutoPlaying = false;
   }
 }
-
-document.querySelector(".js-rock-button").addEventListener("click", () => {
-  playGame("rock");
-});
-
-document.querySelector(".js-paper-button").addEventListener("click", () => {
-  playGame("paper");
-});
-
-document.querySelector(".js-scissors-button").addEventListener("click", () => {
-  playGame("scissors");
-});
 
 document.body.addEventListener("keydown", (event) => {
   if (event.key === "r") {
