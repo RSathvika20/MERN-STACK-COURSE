b07a462eedcd68c6c0a53fc280fa75d9f6e51784
RSathvika20 <7vikarodda@gmail.com>
2025-05-29 20:41:33 +0530

    modify

--- JavaScript/lesson12AdvancedFunctions/rps.js
+++ JavaScript/lesson12AdvancedFunctions/rps.js
@@ -36,16 +36,6 @@ function autoPlay() {
   }
 }
 
-document.body.addEventListener("keydown", (event) => {
-  if (event.key === "r") {
-    playGame("rock");
-  } else if (event.key === "p") {
-    playGame("paper");
-  } else if (event.key === "s") {
-    playGame("scissors");
-  }
-});
-
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
