6fc7a59d2d2890070707b67d96d44b9b178e5963
RSathvika20 <7vikarodda@gmail.com>
2025-06-01 21:00:54 +0530

    changes from testing

--- JavaScript/Amazonproject/scripts/amazon.js
+++ JavaScript/Amazonproject/scripts/amazon.js
@@ -1,7 +1,7 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
-const cart = [];
+
 let productsHTML = "";
 
 products.forEach((product) => {
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-      $${formatCurrency(product.priceCents)}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
